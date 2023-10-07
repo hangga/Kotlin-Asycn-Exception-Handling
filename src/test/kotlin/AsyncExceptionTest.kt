765727ffc785303ceae5e4ba519971c8352b1484
@hangga <bazeniancode@gmail.com>
2023-10-07 20:28:29 +0700

    sip

--- src/test/kotlin/AsyncExceptionTest.kt
+++ src/test/kotlin/AsyncExceptionTest.kt
@@ -1,37 +1,59 @@
-import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.io.File
 import java.io.IOException
+import kotlin.random.Random
 
 
 // https://stackoverflow.com/questions/53303358/kotlin-async-exception-handling
 
 class AsyncExceptionTest {
-    @Throws(IOException::class)
-    suspend fun readFile(fileName: String): String {
-        val file = File(fileName)
-        if (!file.exists()) {
-            throw IOException("File $fileName tidak ditemukan.")
-        }
 
-        return file.readText()
+    fun fetchData(id: Int): String {
+        if (Random.nextBoolean()) {
+            throw IllegalArgumentException("Exception for id $id")
+        }
+        return "Data for id $id"
     }
 
     @Test
-    fun testReadFile() = runBlocking {
-        val job1 = async { readFile("sample.txt") }
-        val job2 = async { readFile("another.txt") }
+    fun withTryCatch(){
+        runBlocking {
+            val deferredResults = List(5) {
+                async {
+                    try {
+                        fetchData(it)
+                    } catch (e: Exception) {
+                        println("Exception for id $it: ${e.message}")
+                        "Error"
+                    }
+                }
+            }
 
-        try {
-            val content1 = job1.await()
-            val content2 = job2.await()
-
-            assertEquals("Sample file content", content1.trim())
-            assertEquals("Another file content", content2.trim())
-        } catch (e: IOException) {
-            println("Terjadi kesalahan: ${e.message}")
+            val results = deferredResults.awaitAll()
+            results.forEachIndexed { index, result ->
+                println("Hasil untuk id $index: $result")
+            }
         }
     }
+
+    @Test
+    fun withAwait(){
+        runBlocking {
+            val deferredResults = List(5) { async { fetchData(it) } }
+
+            try {
+                val results = awaitAll(*deferredResults.toTypedArray())
+                results.forEachIndexed { index, result ->
+                    println("Hasil untuk id $index: $result")
+                }
+            } catch (e: Exception) {
+                println("Exception ditangani: ${e.message}")
+            }
+        }
+    }
+
+
 }
